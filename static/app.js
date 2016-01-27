adb882234b8cd2366b1a3c6c3e494e457c954987
Max Gurela <maxpowa1@gmail.com>
2016-01-27 14:04:59 -0800

    Clean up JS

--- static/app.js
+++ static/app.js
@@ -1,87 +1,84 @@
 var irclogger = {
-  shift: false
+    shift: false
 }
 
 $(document).ready(function() {
-  var hash = window.location.hash.substring(1)
+    var hash = window.location.hash.substring(1);
 
-  if (hash.length > 0) {
-    $(document.body).addClass("highlight")
+    if (hash.length > 0) {
+        $(document.body).addClass("highlight");
 
-    if (hash.match(/^[0-9]/)) {
-      var anchors = hash.split("-")
-      highlightLines(anchors)
-    }
-    else {
-      var anchors = hash.split(",")
-      highlightNicknames(anchors)
-    }
+        var anchors;
+        if (hash.match(/^[0-9]/)) {
+            anchors = hash.split("-");
+            highlightLines(anchors);
+        } else {
+            anchors = hash.split(",");
+            highlightNicknames(anchors);
+        }
 
-    if (anchors.length > 1) $("a[name='" + anchors[0] + "']")[0].scrollIntoView()
-  }
-
-  $("#log a[name]").click(function() {
-    if (irclogger.shift) {
-      var from = window.location.hash.substring(1).split("-")[0]
-      var to = this.name
-      highlightLines([from, to])
-      window.location.hash = "#" + from + "-" + to
-    }
-    else {
-      $("#log > div").removeClass("highlight")
-      $(this).parent().addClass("highlight")
-      window.location.hash = "#" + this.name
+        if (anchors.length > 1) $("a[name='" + anchors[0] + "']")[0].scrollIntoView();
     }
 
-    return false
-  })
+    $("#log a[name]").click(function() {
+        if (irclogger.shift) {
+            var from = window.location.hash.substring(1).split("-")[0];
+            var to = this.name;
+            highlightLines([from, to]);
+            window.location.hash = "#" + from + "-" + to;
+        } else {
+            $("#log > div").removeClass("highlight");
+            $(this).parent().addClass("highlight");
+            window.location.hash = "#" + this.name;
+        }
 
-  $(document).keydown(function(e) {
-    if (e.keyCode == 16) irclogger.shift = true
-  })
+        return false;
+    });
 
-  $(document).keyup(function(e) {
-    if (e.keyCode == 16) irclogger.shift = false
-  })
-})
+    $(document).keydown(function(e) {
+        if (e.keyCode == 16) irclogger.shift = true;
+    });
+
+    $(document).keyup(function(e) {
+        if (e.keyCode == 16) irclogger.shift = false;
+    });
+});
 
 function highlightLines(range) {
-  $(document.body).addClass("highlight")
+    $(document.body).addClass("highlight");
 
-  range = range.sort()
+    range = range.sort();
 
-  var first = range[0]
-  var last  = range[1] || range[0]
+    var first = range[0];
+    var last = range[1] || range[0];
 
-  $("#log a").each(function() {
-    var $entry = $(this).parent()
+    $("#log a").each(function() {
+        var $entry = $(this).parent();
 
-    if (this.name >= first && this.name <= last) {
-      $entry.addClass("highlight")
-    }
-    else {
-      $entry.removeClass("highlight")
-    }
-  })
+        if (this.name >= first && this.name <= last) {
+            $entry.addClass("highlight");
+        } else {
+            $entry.removeClass("highlight");
+        }
+    });
 }
 
 function highlightNicknames(nicknames) {
-  nicknames = $.map(nicknames, function(nickname) {
-    return "<" + nickname + ">"
-  })
+    nicknames = $.map(nicknames, function(nickname) {
+        return "<" + nickname + ">";
+    });
 
-  $("#log .msg .nickname").each(function() {
-    var $entry = $(this)
+    $("#log .msg .nickname").each(function() {
+        var $entry = $(this);
 
-    $.each(nicknames, function() {
-      var nickname = this
+        $.each(nicknames, function() {
+            var nickname = this;
 
-      if ($entry.text().indexOf(nickname) > 0) {
-        $entry.parent().addClass("highlight")
+            if ($entry.text().indexOf(nickname) >= 0) {
+                $entry.parent().addClass("highlight");
 
-        return
-      }
-    })
-  })
+                return;
+            }
+        });
+    });
 }
-
